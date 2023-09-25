2eaa77b756e360d4c7f9e7da24ea92e51e13d851
Medet Hasan Ugurlu <mdt.hsn.76@hotmail.com>
2023-09-25 18:18:13 +0300

    first commit

---
+++ black-hole/src/test/java/com/gallifreyan/blackhole/BlackHoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallifreyan.blackhole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackHoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
